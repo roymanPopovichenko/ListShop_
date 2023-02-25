22792e9d500c4fac5dafa69b5501a726356c427c
roman <aregsa2@gmail.com>
2023-02-25 13:22:40 +0200

    test push

--- app/src/main/java/com/example/listshop/Domain/EditShopItemUseCase.kt
+++ app/src/main/java/com/example/listshop/Domain/EditShopItemUseCase.kt
@@ -2,6 +2,8 @@ package com.example.listshop.Domain
 
 class EditShopItemUseCase(val repository: ShopListRepository) {
 
+    private val name = "xyi"
+
     fun editShopItem(shopItem: ShopItem) {
         repository.editShopItem(shopItem)
     }
